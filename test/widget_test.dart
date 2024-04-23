2cb03cef860f561c6c6ced2bee47ad1eed0c9fe6
Aiteam55 <mitiaandria1@gmail.com>
2024-04-23 21:27:15 +0300

    Add new test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_cicd/main.dart';
+void main() {
+  group('Simple Math Test', () {
+    test('adds 1 + 2 to equal 3', () {
+      final result = add(1, 2);
+      expect(result, 3);
+    });
+  });
+}
 
-void main() {}
+// This function is not part of the test, it's just an example
+int add(int a, int b) {
+  return a + b;
+}
